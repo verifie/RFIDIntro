f4b5685fd8ebf683c6a13484a054ccedf74b0fe2
Paul Emerton <paulemerton@gmail.com>
2020-04-25 22:22:24 +0100

    First commit

---
+++ RFIDTrialCode.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+#
+
+#########################################################################################################################################    
+# RFIDTrialCode.py
+# 
+# RFID Project
+# (c) 2020 Leighton Electronics
+# 
+# Description :         RFID Raspberry Pi / Python 3 / RFID-RC522.
+#
+# Status :              25 - Start
+#
+# Version History
+# 2019/04/25 2211 V1.00 PME - Trial Code.
+#
+
+
+# Simple print screen introduction
+print("")
+print("=============================================================================================================================================")
+print("")
+print(",------. ,------.,--.,------.   ")
+print("|  .--. '|  .---'|  ||  .-.  \  ")
+print("|  '--'.'|  `--, |  ||  |  \  : ")
+print("|  |\  \ |  |`   |  ||  '--'  / ")
+print("`--' '--'`--'    `--'`-------'  ")
+print("                                ")
+print(" Copyright (c) Leighton Electronics 2020 Onwards. Patent Pending. NO UNAUTHORISED ACCESS PERMITTED. ")
+print(" www.LeightonElectronics.co.uk ")
+print("")
+print("=============================================================================================================================================")
+print("")
+print("")
+
+
+# Import Libraries.
+import inspect
+
+
